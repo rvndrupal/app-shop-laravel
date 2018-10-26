a63bf35324823d362b4c1edf2818e1dd5ed3690d
Rodrigo Igor Villanueva Nieto <rodrigo.villanueva.c@senasica.gob.mx>
2018-10-26 11:34:13 -0500

    Hasta la vista login falta menus

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,7 @@
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard</div>
+               
 
                 <div class="panel-body">
                     @if (session('status'))
